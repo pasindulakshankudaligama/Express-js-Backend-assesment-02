38aaadd10f16c783cf490ee37e6cbe8c95fcf7a9
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-23 00:10:45 +0530

    get customer method added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const customer = require("./routes/customer");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
+
+app.use("/customer", customer);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
