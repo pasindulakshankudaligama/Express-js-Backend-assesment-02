daea4ef1c1d364a41493fdb5795b84022ed15e77
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-24 12:57:53 +0530

    item delete method completed

--- routes/Item.js
+++ routes/Item.js
@@ -5,6 +5,7 @@ const db =require("../configs/db.config");
 const mysql =require("mysql");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+const Connection = require("mysql/lib/Connection");
 const connection = mysql.createConnection(db.database);
 
 connection.connect(function(err){
@@ -36,7 +37,47 @@ router.post("/",(req,res)=>{
     const qty = req.body.qty;
     const price = req.body.price;
 
-    const query = ""
+    const query = "INSERT INTO item VALUES(?,?,?,?)"
+
+    connection.query(query,[id,name,qty,price],(err)=>{
+        if(err){
+            res.send({message:"Duplicate entry"});
+        }else{
+            res.send({message:"item created"});
+        }
+    });
+});
+
+router.put("/",(req,res)=>{
+    const id = req.body.id;
+    const name = req.body.name;
+    const qty = req.body.qty;
+    const price = req.body.price;
+
+    const query = " UPDATE item SET name=?, qty=?, price=? WHERE id=?";
+
+    connection.query(query,[name,qty,price,id],(err,rows)=>{
+        if (err) throw err;
+        if (rows.affectedRows>0){
+            res.send({message:"Item updated"});
+        }else{
+            res.send({message:"Item not found"});
+        }
+    });
+});
+
+router.delete("/:id",(req,res)=>{
+    const id = req.params.id;
+    const query = "DELETE FROM item WHERE id=?";
+  
+    connection.query(query, [id], (err, rows) => {
+      if (err) throw err;
+      if (rows.affectedRows > 0) {
+        res.send({ message: "Item Deleted" });
+      } else {
+        res.send({ message: "Item Not Found" });
+      }
+    });
 });
 
 module.exports = router;
