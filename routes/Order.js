f335722fdd75cea743d2d9803a20bffdfa9aa9ed
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-24 13:14:51 +0530

    get method added to ordee

--- routes/Order.js
+++ routes/Order.js
@@ -21,6 +21,14 @@ connection.connect(function (err) {
   }
 });
 
+router.get("/", (req, res) => {
+  const query = "SELECT * FROM orders";
+  connection.query(query, (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
 
 
 module.exports = router;
