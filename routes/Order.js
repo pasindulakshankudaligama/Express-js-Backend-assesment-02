2eba695c4aac334f676652a08461ce43b8395827
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-24 13:13:46 +0530

    ordr table added

---
+++ routes/Order.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../configs/db.config");
+
+const mysql = require("mysql");
+const connection = mysql.createConnection(db.database);
+
+connection.connect(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connected to the mysql server");
+    var orderTbl =
+      "CREATE TABLE IF NOT EXISTS Orders(id VARCHAR(255) PRIMARY KEY, date VARCHAR(255), customer_id VARCHAR(255))";
+    connection.query(orderTbl, function (err, result) {
+      if (err) throw err;
+      if (result.warningCount === 0) {
+        console.log("Order table created");
+      }
+    });
+  }
+});
+
+
+
+module.exports = router;
