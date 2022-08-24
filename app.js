35e81f0acbadabe014892a164f94af0ff3bb6548
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-24 13:20:33 +0530

    get all order detail method added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const customer = require("./routes/Customer");
 const item = require("./routes/Item");
 const order = require("./routes/Order");
+const orderDetails = require("./routes/OrderDetails");
 const app = express();
 const port = 5600;
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/customer", customer);
 app.use("/item", item);
 app.use("/order",order);
+app.use("/orderdetails", orderDetails);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
