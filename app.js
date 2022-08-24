e8f62c5db590de47a339f2ca3e655acd33c43e4c
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-24 13:00:01 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const customer = require("./routes/Customer");
 const item = require("./routes/Item");
+const order = require("./routes/Order");
 const app = express();
 const port = 5000;
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use("/customer", customer);
 app.use("/item", item);
+app.use("/order", order);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
